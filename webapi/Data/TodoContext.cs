37a50c72c50d3e8b81630f43bdffa8fe7c458e1d
John Nguyen <clear.action.consistent@gmail.com>
2024-12-31 14:07:29 +0700

    add todo dbcontext

---
+++ webapi/Data/TodoContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using webapi.Models;
+
+namespace webapi.Data
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; } = null!;
+    }
+}
