0ec32e51b6e6abfa026e1a1b189f65d6f72c257d
John Nguyen <clear.action.consistent@gmail.com>
2025-01-02 13:39:28 +0700

    Config Identity system: DB & Cookie

--- AspNetCoreIdentityRazor/Program.cs
+++ AspNetCoreIdentityRazor/Program.cs
@@ -1,4 +1,5 @@
 using AspNetCoreIdentityRazor.Data;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +8,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<IdentityDBContext>(options => {
     options.UseSqlServer(builder.Configuration.GetSection("SQLDBConnectionStrings")["Default"]);
+});
+
+//we can specify other classes to get extra fields of user/role
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => {
+    options.Password.RequiredLength = 9;
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.User.RequireUniqueEmail = true;
+})
+.AddEntityFrameworkStores<IdentityDBContext>();
+
+builder.Services.ConfigureApplicationCookie(options => {
+    options.LoginPath = "/Account/Login";
+    options.AccessDeniedPath = "/Account/AccessDenied";
 });
 
 
@@ -20,6 +34,7 @@ if (!app.Environment.IsDevelopment())
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapStaticAssets();
