f959f8a86ca78f1dda86c4b013b674b86898f29c
John Nguyen <clear.action.consistent@gmail.com>
2025-01-02 13:18:11 +0700

    Config EF core , Migration & setup Identity Context

--- AspNetCoreIdentityRazor/Program.cs
+++ AspNetCoreIdentityRazor/Program.cs
@@ -1,7 +1,14 @@
+using AspNetCoreIdentityRazor.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<IdentityDBContext>(options => {
+    options.UseSqlServer(builder.Configuration.GetSection("SQLDBConnectionStrings")["Default"]);
+});
+
 
 var app = builder.Build();
 
